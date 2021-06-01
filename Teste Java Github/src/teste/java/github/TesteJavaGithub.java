118401cd03d9ca8027517a36be99ccc3c92cfc99
Matheus Giovanni Pires <56092403+mgpires@users.noreply.github.com>
2021-06-01 11:44:24 -0300

    Update TesteJavaGithub.java

--- Teste Java Github/src/teste/java/github/TesteJavaGithub.java
+++ Teste Java Github/src/teste/java/github/TesteJavaGithub.java
@@ -15,7 +15,7 @@ public class TesteJavaGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste 1 Java Github 05/01/2021");
+        System.out.println("Teste 1 Java Github 01/06/2021");
         
         System.out.println("Teste 2 Java Github");
         
